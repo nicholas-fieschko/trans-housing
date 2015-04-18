0560a3d71738dd6a00872f06f9ee29edda7e7b94
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-18 16:00:37 -0400

    added testwilio SMS

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -24,4 +24,22 @@ def mailgun(user)
   mg_client.send_message ENV['domain'], message_params
 end
 
+# Ruby wrapper for Twilio; send SMS copies of emails
+# Keep everything text only
+def twilio(user)
+  @user = user
+  account_sid = ENV['account_sid']
+  auth_token  = ENV['auth_token']
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  # Once client is talking to API, just send proof of concept text
+  @client.messages.create(
+    from: ENV['twilio_num'],
+    to: '+15189283302',
+    body: 'A test text from TransHousing!'
+  )
+  # Do twilio stuff here. Need to get convention over config stuff
+  # working to DRY out the message (don't use :text)
+end
+
 end
