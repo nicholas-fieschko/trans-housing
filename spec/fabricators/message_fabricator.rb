4ff763fad901bbf7a82fee3826b861f09af0c291
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-19 12:50:41 -0400

    adding some tests

--- spec/fabricators/message_fabricator.rb
+++ spec/fabricators/message_fabricator.rb
@@ -1,6 +1,6 @@
 # Ensure that when Fabricate builds a message, it has enough text and that it
-# has an author (just grab a random User)
-Fabricator(:message) do
+# has an author (just grab a random User). Make sure it inherits from Convo.
+Fabricator(:message, from: :conversation) do
 	text	 { Faker::Lorem.paragraph }
-	author { User.last.id }
+	author { |attrs| attrs[:participants][1] }
 end
