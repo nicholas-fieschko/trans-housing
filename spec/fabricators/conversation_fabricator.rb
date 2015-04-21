e5c1019a34cda250140791c313a421016f9b6430
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-20 21:29:21 -0400

    ok now fabricator is creating multiple messages in the thread

--- spec/fabricators/conversation_fabricator.rb
+++ spec/fabricators/conversation_fabricator.rb
@@ -1,14 +1,21 @@
 Fabricator(:conversation) do
 
+	# Get setup with some temporary users
 	transient sender: 		Fabricate.build(:user)
 	transient receiver:		Fabricate.build(:user)
 
+	# Pass a block to insert their info into users array
 	users									{ |attrs| [ attrs[:sender], attrs[:receiver] ] }
 
-	messages							{ |attrs| [
-																		Fabricate(:message,
-																						 	sender: attrs[:sender].id,
-																							receiver: attrs[:receiver].id)
-																	] 
+	# Make a message
+	messages(count: 4)		{ |attrs, i|
+														x = rand(2)
+														(x == 1) ? (y = 0) : (y = 1)
+														Fabricate(:message,
+																			 sender:	 attrs[:users][x].id,
+																			 receiver: attrs[:users][y].id)
+																			 #sender: attrs[:sender].id,
+																			 #receiver: attrs[:receiver].id)
 												}
+
 end
