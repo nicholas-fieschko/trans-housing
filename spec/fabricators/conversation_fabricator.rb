c5c440c9fecccff57154aa56871e9d8b405a1f6d
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-20 21:41:35 -0400

    need to decide if receivers are 1 or N

--- spec/fabricators/conversation_fabricator.rb
+++ spec/fabricators/conversation_fabricator.rb
@@ -4,6 +4,10 @@ Fabricator(:conversation) do
 	transient sender: 		Fabricate.build(:user)
 	transient receiver:		Fabricate.build(:user)
 
+	# OK, work on having multiple receivers AFTER getting views down...
+	# pass a rand count to users and then set a sender in messages; the
+	# receivers will just be everybody else
+
 	# Pass a block to insert their info into users array
 	users									{ |attrs| [ attrs[:sender], attrs[:receiver] ] }
 
