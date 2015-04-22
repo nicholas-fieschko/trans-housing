382b827bd94936a180108840c6c885a408e89259
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-22 03:10:02 -0400

    ok so the message sending is working--create the show action

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,20 +10,37 @@ class ConversationsController < ApplicationController
 	# Gather form info for the new conversation; send the message to whatever
 	# profile we are viewing--this will be in params as :user_id
 	def new
+		@user     = current_user
 		@receiver = User.find(params[:user_id])
-		@conversation = Conversation.create(:user_ids => [@receiver.id, current_user.id])
-		
-		# Persist both users so that they have the conversation now
-		@receiver.save
-		current_user.save
+
+		@conversation = Conversation.create(user_ids: [@user.id, @receiver.id])
+		@conversation.messages.push(Message.new(sender: current_user.id, receiver: @receiver.id))
+
+		#@user.save
+		#@receiver.save
+		#@conversation.save
 	end
 
 	# Create that new conversation, persist, and redirect to inbox
 	def create
-		@user = User.find(params[:user_id])
-	
+		@user     = current_user
+		@receiver = User.find(params[:user_id])
+
+		@conversation = Conversation.new(
+			user_ids: [@user.id, @receiver.id],
+			subject: params[:conversation][:subject]
+		)
+		@message = Message.new(
+			sender: @user.id,
+			receiver: @receiver.id,
+			text: params[:message][:text]
+		)
+		
+		@conversation.messages.push(@message)
 		@user.save
-		current_user.save
+		@receiver.save
+		@conversation.save
+
 		redirect_to user_conversations_path
 	end
 
@@ -42,10 +59,15 @@ class ConversationsController < ApplicationController
 
 	end
 
-	# Rails 4 strong params stuff
+	# Rails 4 Strong Params
 	private
-	def conversation_params
-		params.require(:conversation).permit(:messages, :subject)
-	end
+
+		# Allow nesting of message model inside conversation
+    def conversation_params
+      params.require(:conversation).permit(
+        :subject,
+        message_attributes: [:sender, :receiver, :text],
+      )
+    end
 
 end
