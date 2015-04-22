cef73df63cdaeeb8063edd464b7879989165bf86
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-22 00:29:24 -0400

    why is conversation getting lost?

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,23 +1,29 @@
 class ConversationsController < ApplicationController
 
-	# Show user's inbox, ordered by most recently updated
+	# Show user's inbox, ordered by most recently updated; where just finds
+	# all conversations in which @user is in the user_ids
 	def index
 		@user = current_user
 		@inbox = Conversation.where(:user_ids => @user.id)
 	end
 
-	# Gather form info for the new conversation
+	# Gather form info for the new conversation; send the message to whatever
+	# profile we are viewing--this will be in params as :user_id
 	def new
-		@user = User.find(params[:user_id])
-		@conversation = Conversation.new(:user_ids => [@user.id, current_user.id])
-		@user.save
+		@receiver = User.find(params[:user_id])
+		@conversation = Conversation.create(:user_ids => [@receiver.id, current_user.id])
+		
+		# Persist both users so that they have the conversation now
+		@receiver.save
 		current_user.save
 	end
 
 	# Create that new conversation, persist, and redirect to inbox
 	def create
 		@user = User.find(params[:user_id])
+	
 		@user.save
+		current_user.save
 		redirect_to user_conversations_path
 	end
 
