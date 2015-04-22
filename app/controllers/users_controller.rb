2f3edee4c0af66982fd3491a694f9106d7a23dce
Nick <nick.fieschko@yale.edu>
2015-04-21 20:25:30 -0400

    merge time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
+      sign_in     @user
       redirect_to @user
     else
       render 'new'
