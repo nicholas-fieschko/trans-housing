69593dc4a5fcccb4a1a174429ab4ba883ae75a25
Shantao Li <shantaolee@gmail.com>
2015-04-25 20:26:29 -0400

    rspec primitivies

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe LocationsController, type: :controller do
-	describe "POST #location/post" do
+	describe "POST #location/posts" do
 		it "responds successfully with an HTTP 200 status" do
-		post :location/post
+		post :location/posts
 		expect(response).to be_success
 		expect(response).to have_http_status(200)
+		end
 	end
 end
