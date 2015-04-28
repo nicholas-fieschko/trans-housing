990eb54bb2512f800125d8c3f471740db7b66efc
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-28 19:45:07 -0400

    handle intl phones

--- config/initializers/global_phone.rb
+++ config/initializers/global_phone.rb
@@ -1,3 +1,3 @@
 require 'global_phone'
 GlobalPhone.db_path                = Rails.root.join('db/global_phone.json')
-GlobalPhone.default_territory_name = :us
+#GlobalPhone.default_territory_name = :us
