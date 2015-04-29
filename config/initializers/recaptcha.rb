45a2c713718d8744d2d34b7d09ca28b868b3b4c1
Stephen Krewson <stephen.krewson@gmail.com>
2015-04-29 04:35:34 -0400

    moved recaptcha keys into figaro

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,6 @@
-
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld1TwUTAAAAAOgV0Vm8UIT7zn-kEiF-Sa3gX6gK'
-  config.private_key = '6Ld1TwUTAAAAAD2mUqF3B-n35QsxoUEd1UPJsAte'
+  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
 	# config.proxy = 'http://myproxy.com:8080'
   # >= version 0.3.7 can use newer API
   config.api_version = 'v2'
