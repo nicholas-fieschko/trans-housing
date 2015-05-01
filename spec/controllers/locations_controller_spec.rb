0921605d78e6180fe0310b43e97f57bc1767eec8
Shantao Li <shantaolee@gmail.com>
2015-05-01 08:40:04 -0400

    add ~15 unit tests in controller/routing

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe LocationsController, type: :controller do
+RSpec.describe LocationsController, :type => :controller do
+	describe "GET index" do
+		it "has a 200 status code" do
+			get :index
+			expect(response.status).to eq(200)
+		end
+	end
+
+	it "renders the index template" do
+	    get :index
+		expect(response).to render_template("index")
+	end
 	# describe "POST #location/posts" do
 	# 	it "responds successfully with an HTTP 201 status" do
 	# 	post :location/posts
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	describe "GET index" do
+        it "has a 200 status code" do
+            get :index
+            expect(response.status).to eq(200)
+        end
+    end
 
-
-
+    it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+    end
+	
 end
